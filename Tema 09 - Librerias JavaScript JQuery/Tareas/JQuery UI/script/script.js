cc38c2aefddcc9c7bb402e4932a629a35149a993
JCristian <73657780+JCristianFedez@users.noreply.github.com>
2021-02-10 16:54:47 +0100

    Ejercicio jquery ui terminado

--- Tema 09 - Librerias JavaScript JQuery/Tareas/JQuery UI/script/script.js
+++ Tema 09 - Librerias JavaScript JQuery/Tareas/JQuery UI/script/script.js
@@ -1,7 +1,74 @@
 let x = $(document);
 x.ready(inicializarEventos);
 
-function inicializarEventos(){
-    $( "#acordeon" ).accordion();
+function inicializarEventos() {
+    widgets();
+    interactions();
+    efects();
+}
 
+function widgets() {
+    $("#accordion").accordion();
+
+    $("#autocomplete").autocomplete({
+        source: ["c++", "java", "php", "coldfusion", "javascript", "asp", "ruby"]
+    });
+
+    $("#button").button();
+    $("#button-icon").button({
+        icon: "ui-icon-gear",
+        showLabel: false
+    });
+
+    $("#radio").buttonset();
+
+    $("#datepicker").datepicker();
+
+    $("#dialog").dialog({ autoOpen: false });
+    $("#opener").click(function () {
+        $("#dialog").dialog("open");
+    });
+
+    $("#menu").menu();
+
+    $("#progressbar").progressbar({
+        value: false
+    });
+
+    $("#slider").slider();
+
+    $("#tabs").tabs();
+
+}
+
+function interactions() {
+    $("#draggable").draggable();
+
+    $("#droppable").droppable({
+        drop: function () {
+            alert("dropped");
+        }
+    });
+
+    $("#resizable").resizable();
+
+    $("#selectable").selectable();
+
+    $("#sortable").sortable();
+}
+
+function efects() {
+    $("#toggleDropTitle").click(function () {
+        $("#toggleDrop").toggle("drop");
+    });
+
+    $( "#toggleScaleTitle" ).click(function() {
+        $( "#toggleScale" ).toggle({ effect: "scale", direction: "horizontal" });
+      });
+
+
+      $( "#toggleExplodeTitle" ).click(function() {
+        $( "#toggleExplode" ).toggle( "explode" );
+      });
+    
 }
